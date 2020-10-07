1d56c11dd6af0539faba58b80393b0280e275942
Wojtek Wojtkiewicz <wojtek.wojtkiewiczlt@gmail.com>
2020-10-07 15:02:31 +0200

    server works but not connected

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import React, { Component } from "react";
-import ButtonAddPerson from "./components/ButtonAddPerson.js.js";
-import People from "./components/People.js.js";
 import { BrowserRouter as Router } from "react-router-dom";
-import Page from "./layouts/Page.js.js";
-import Footer from "./layouts/Footer";
-import Header from "./layouts/Header";
-import "./styles/Header.css";
 import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <div className="app"></div>
-      </Router>
-    );
-  }
-}
+const App = () => {
+  return (
+    <Router>
+      <div className="app">wadaw</div>
+    </Router>
+  );
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App.js";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
 import { rouletteReducer } from "./reducers/rouletteReducer";
+import App from "./App.js";
 import "bootstrap/dist/css/bootstrap.css";
 
 const store = createStore(rouletteReducer);
